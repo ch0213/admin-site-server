c516b46e83642b03cb0eeb3336127b1fd621fd26
chkim <cndghks0213@gmail.com>
2022-03-27 04:41:03 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/admin/adminsiteserver/member/member/domain/MemberRepository.java
@@ -0,0 +1,6 @@
+package admin.adminsiteserver.member.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
