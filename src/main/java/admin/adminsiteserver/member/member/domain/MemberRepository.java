80acc4a1631449efd495ab85487b9cdec2f3d4eb
chkim <cndghks0213@gmail.com>
2022-03-27 07:13:24 +0900

    feat: email로 회원을 찾는 기능 추가

--- src/main/java/admin/adminsiteserver/member/member/domain/MemberRepository.java
+++ src/main/java/admin/adminsiteserver/member/member/domain/MemberRepository.java
@@ -2,5 +2,9 @@ package admin.adminsiteserver.member.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
