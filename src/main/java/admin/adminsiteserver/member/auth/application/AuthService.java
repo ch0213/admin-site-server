a25b7258f8a4c30aec687a52ce2f2dc5b0c5df97
chkim <cndghks0213@gmail.com>
2022-03-29 00:11:50 +0900

    feat: login 서비스 구현

---
+++ src/main/java/admin/adminsiteserver/member/auth/application/AuthService.java
@@ -0,0 +1,40 @@
+package admin.adminsiteserver.member.auth.application;
+
+import admin.adminsiteserver.member.auth.application.dto.JwtTokenDto;
+import admin.adminsiteserver.member.auth.application.dto.LoginResponse;
+import admin.adminsiteserver.member.auth.exception.NotExistMemberException;
+import admin.adminsiteserver.member.auth.exception.WrongPasswordException;
+import admin.adminsiteserver.member.auth.ui.dto.LoginRequest;
+import admin.adminsiteserver.member.auth.util.JwtTokenProvider;
+import admin.adminsiteserver.member.member.domain.Member;
+import admin.adminsiteserver.member.member.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import static admin.adminsiteserver.member.auth.exception.AuthExceptionType.*;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        Member member = memberRepository.findByUserId(loginRequest.getUserId())
+                .orElseThrow(() -> new NotExistMemberException(NOT_EXIST_MEMBER));
+
+        if (isWrongPassword(loginRequest, member)) {
+            throw new WrongPasswordException(WRONG_PASSWORD);
+        }
+
+        JwtTokenDto tokens = jwtTokenProvider.createTokens(member);
+        return LoginResponse.of(member, tokens);
+    }
+
+    private boolean isWrongPassword(LoginRequest loginRequest, Member member) {
+        return !passwordEncoder.matches(loginRequest.getPassword(), member.getPassword());
+    }
+}
