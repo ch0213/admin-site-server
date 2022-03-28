5237405945cb1a23a6747a73af11f8be223799b1
chkim <cndghks0213@gmail.com>
2022-03-29 03:30:45 +0900

    refactor: CommonResponse 정적 팩토리 메서드명 변경

--- src/main/java/admin/adminsiteserver/member/auth/ui/AuthController.java
+++ src/main/java/admin/adminsiteserver/member/auth/ui/AuthController.java
@@ -6,7 +6,6 @@ import admin.adminsiteserver.member.auth.application.dto.LoginResponse;
 import admin.adminsiteserver.member.auth.ui.dto.LoginRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +21,6 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<CommonResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest) {
         LoginResponse loginResponse = authService.login(loginRequest);
-        return ResponseEntity.ok(CommonResponse.from(loginResponse, LOGIN_SUCCESS.getMessage()));
+        return ResponseEntity.ok(CommonResponse.of(loginResponse, LOGIN_SUCCESS.getMessage()));
     }
 }
