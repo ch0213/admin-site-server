66dac567cf79825370d72cd550af353dfcf4ba2a
chkim <cndghks0213@gmail.com>
2022-03-29 04:54:09 +0900

    feat: header의 jwt token으로부터 사용자의 정보를 얻는 JwtTokenArgumentResolver 구현

---
+++ src/main/java/admin/adminsiteserver/common/config/WebConfig.java
@@ -0,0 +1,29 @@
+package admin.adminsiteserver.common.config;
+
+import admin.adminsiteserver.member.auth.util.JwtTokenArgumentResolver;
+import admin.adminsiteserver.member.auth.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new JwtTokenArgumentResolver(jwtTokenProvider));
+    }
+}
---
+++ src/main/java/admin/adminsiteserver/member/auth/util/dto/LoginUserInfo.java
@@ -0,0 +1,30 @@
+package admin.adminsiteserver.member.auth.util.dto;
+
+import io.jsonwebtoken.Claims;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+public class LoginUserInfo {
+    private static final String USER_ID = "userId";
+    private static final String EMAIL = "email";
+    private static final String NAME = "name";
+    private static final String ROLE = "role";
+
+    private String userId;
+    private String email;
+    private String name;
+    private String role;
+
+    public static LoginUserInfo from(Claims claims) {
+        return new LoginUserInfo(
+                (String) claims.get(USER_ID),
+                (String) claims.get(EMAIL),
+                (String) claims.get(NAME),
+                (String) claims.get(ROLE)
+        );
+    }
+}
