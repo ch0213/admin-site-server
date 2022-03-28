c21e4df0f442259a7dc985dd55d20f044493fbbb
chkim <cndghks0213@gmail.com>
2022-03-29 00:06:51 +0900

    fix: 로그인, 회원가입 요청 허용하도록 변경

--- src/main/java/admin/adminsiteserver/common/config/SecurityConfig.java
+++ src/main/java/admin/adminsiteserver/common/config/SecurityConfig.java
@@ -1,19 +1,23 @@
 package admin.adminsiteserver.common.config;
 
+import admin.adminsiteserver.common.filter.JwtAuthenticationFilter;
+import admin.adminsiteserver.member.auth.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -25,9 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .httpBasic().disable()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/login", "/signup").permitAll()
+                .anyRequest().authenticated();
     }
 }
