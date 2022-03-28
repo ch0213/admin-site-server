a0aad0e982ce0c580c5ebdf5692151425d8ca65c
chkim <cndghks0213@gmail.com>
2022-03-29 00:10:31 +0900

    fix: email이 아닌 userid로 중복검사를 하도록 수정

--- src/main/java/admin/adminsiteserver/member/member/exception/MemberExceptionType.java
+++ src/main/java/admin/adminsiteserver/member/member/exception/MemberExceptionType.java
@@ -9,7 +9,7 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @AllArgsConstructor
 public enum MemberExceptionType {
-    ALREADY_EXIST_EMAIL("이미 존재하는 회원입니다.", BAD_REQUEST);
+    ALREADY_EXIST_USER_ID("이미 존재하는 아이디입니다.", BAD_REQUEST);
 
     private final String message;
     private HttpStatus status;
