55c7caf2535da716531530943b6823dcb2d88510
chkim <cndghks0213@gmail.com>
2022-03-29 00:11:24 +0900

    feat: login 컨트롤러 구현

---
+++ src/main/java/admin/adminsiteserver/member/auth/ui/AuthController.java
@@ -0,0 +1,27 @@
+package admin.adminsiteserver.member.auth.ui;
+
+import admin.adminsiteserver.common.dto.CommonResponse;
+import admin.adminsiteserver.member.auth.application.AuthService;
+import admin.adminsiteserver.member.auth.application.dto.LoginResponse;
+import admin.adminsiteserver.member.auth.ui.dto.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static admin.adminsiteserver.member.auth.ui.AuthResponseMessage.LOGIN_SUCCESS;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<CommonResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest) {
+        LoginResponse loginResponse = authService.login(loginRequest);
+        return ResponseEntity.ok(CommonResponse.from(loginResponse, LOGIN_SUCCESS.getMessage()));
+    }
+}
