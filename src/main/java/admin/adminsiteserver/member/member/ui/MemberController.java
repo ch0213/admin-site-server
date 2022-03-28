e70f61a4529f5dede142751d9560e463a80058bc
chkim <cndghks0213@gmail.com>
2022-03-28 23:47:31 +0900

    refactor: spring security 설정을 위해 요청 url 변경

--- src/main/java/admin/adminsiteserver/member/member/ui/MemberController.java
+++ src/main/java/admin/adminsiteserver/member/member/ui/MemberController.java
@@ -5,6 +5,7 @@ import admin.adminsiteserver.member.member.application.MemberService;
 import admin.adminsiteserver.member.member.application.dto.MemberDto;
 import admin.adminsiteserver.member.member.ui.dto.SignUpRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/member")
+    @PostMapping("/signup")
     public CommonResponse<MemberDto> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
         return CommonResponse.from(memberService.signUp(signUpRequest), SIGNUP_SUCCESS.getMessage());
+    }
+
+    @GetMapping("/auth-test")
+    public CommonResponse<Void> authTest() {
+        return CommonResponse.from("JWT 토큰이 유효합니다.");
     }
 }
