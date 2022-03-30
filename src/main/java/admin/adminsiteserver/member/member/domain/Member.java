9f6a320a03e093745e3a16c325cf0a4e89ecd16f
chkim <cndghks0213@gmail.com>
2022-03-30 23:13:23 +0900

    refactor: Member 생성자에 filePath 추가, 연관관계 매핑 설정

--- src/main/java/admin/adminsiteserver/member/member/domain/Member.java
+++ src/main/java/admin/adminsiteserver/member/member/domain/Member.java
@@ -1,11 +1,13 @@
 package admin.adminsiteserver.member.member.domain;
 
+import admin.adminsiteserver.common.domain.FilePath;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+import static javax.persistence.CascadeType.*;
 import static javax.persistence.EnumType.*;
 import static lombok.AccessLevel.*;
 
@@ -23,17 +25,21 @@ public class Member {
     private String studentNumber;
     private String phoneNumber;
 
+    @OneToOne(mappedBy = "member", cascade = ALL, orphanRemoval = true)
+    private FilePath filePath;
+
     @Enumerated(STRING)
     private RoleType role;
 
     @Builder
-    public Member(String userId, String password, String email, String name, String studentNumber, String phoneNumber, RoleType role) {
+    public Member(String userId, String password, String email, String name, String studentNumber, String phoneNumber, FilePath filePath, RoleType role) {
         this.userId = userId;
         this.password = password;
         this.email = email;
         this.name = name;
         this.studentNumber = studentNumber;
         this.phoneNumber = phoneNumber;
+        this.filePath = filePath;
         this.role = role;
     }
 
@@ -42,5 +48,10 @@ public class Member {
         this.name = name;
         this.studentNumber = studentNumber;
         this.phoneNumber = phoneNumber;
+    }
+
+    public void addProfileImage(FilePath filePath) {
+        this.filePath = filePath;
+        filePath.includedToMember(this);
     }
 }
