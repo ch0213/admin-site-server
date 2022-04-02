058a73d1e1ac1057b1bac653590aa74536023fc0
chkim <cndghks0213@gmail.com>
2022-04-02 23:54:12 +0900

    feat: qna, question, answer Repository 생성

---
+++ src/main/java/admin/adminsiteserver/qna/domain/AnswerRepository.java
@@ -0,0 +1,6 @@
+package admin.adminsiteserver.qna.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
---
+++ src/main/java/admin/adminsiteserver/qna/domain/QuestionRepository.java
@@ -0,0 +1,6 @@
+package admin.adminsiteserver.qna.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
