e46ceb9387992e61ba6bb0dca09f1b117dd772e5
chkim <cndghks0213@gmail.com>
2022-04-03 19:13:41 +0900

    feat: MemberFileRepository 생성

--- src/main/java/admin/adminsiteserver/common/domain/FilePathRepository.java
+++
@@ -1,6 +0,0 @@
-package admin.adminsiteserver.common.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilePathRepository extends JpaRepository<FilePath, Long> {
-}
