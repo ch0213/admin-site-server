66f6bd7c5591ee1b9f6035ba27c82ac781286c1e
chkim <cndghks0213@gmail.com>
2022-04-03 19:10:37 +0900

    fix: Question repository 제거
    - Question, Answer을 나누려 했지만 Question은 글 자체에 포함될 수 있으므로 제거

--- src/main/java/admin/adminsiteserver/qna/domain/QuestionRepository.java
+++
@@ -1,6 +0,0 @@
-package admin.adminsiteserver.qna.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
