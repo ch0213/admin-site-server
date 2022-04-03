a633ee95683ec61c382455a6395c0c2a71c00cbb
chkim <cndghks0213@gmail.com>
2022-04-03 19:11:05 +0900

    feat: 질문사진 엔티티 생성

---
+++ src/main/java/admin/adminsiteserver/qna/domain/QuestionFilePath.java
@@ -0,0 +1,36 @@
+package admin.adminsiteserver.qna.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.*;
+
+@Slf4j
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class QuestionFilePath {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String fileName;
+    private String fileUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "qna")
+    private Qna qna;
+
+    public QuestionFilePath(String fileName, String fileUrl) {
+        this.fileName = fileName;
+        this.fileUrl = fileUrl;
+    }
+
+    public void includedToAnnouncement(Qna qna) {
+        this.qna = qna;
+    }
+}
