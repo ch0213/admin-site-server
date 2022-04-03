00e3f41e2fed76e9b625caef1bbcb8d0b62c4f11
chkim <cndghks0213@gmail.com>
2022-04-03 22:25:47 +0900

    feat: loginUser 정보를 받아 Qna를 생성하는 메서드 구현

--- src/main/java/admin/adminsiteserver/qna/ui/dto/UploadQnaRequest.java
+++ src/main/java/admin/adminsiteserver/qna/ui/dto/UploadQnaRequest.java
@@ -1,5 +1,7 @@
 package admin.adminsiteserver.qna.ui.dto;
 
+import admin.adminsiteserver.member.auth.util.dto.LoginUserInfo;
+import admin.adminsiteserver.qna.domain.Qna;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,4 +14,13 @@ public class UploadQnaRequest {
     private String title;
     private String content;
     private List<MultipartFile> images;
+
+    public Qna createQna(LoginUserInfo loginUserInfo) {
+        return Qna.builder()
+                .authorId(loginUserInfo.getUserId())
+                .authorName(loginUserInfo.getName())
+                .title(title)
+                .content(content)
+                .build();
+    }
 }
