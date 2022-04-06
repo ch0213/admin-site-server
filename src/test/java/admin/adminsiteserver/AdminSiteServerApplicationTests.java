5573f1fbaa7e3f63bca04e0b94b50043d2ee55e1
chkim <cndghks0213@gmail.com>
2022-04-07 05:03:58 +0900

    test: main test에 test profile 추가

--- src/test/java/admin/adminsiteserver/AdminSiteServerApplicationTests.java
+++ src/test/java/admin/adminsiteserver/AdminSiteServerApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AdminSiteServerApplicationTests {
 
