12692d1d8e141342ff37a2007143140e3e63f73b
chkim <cndghks0213@gmail.com>
2022-04-07 05:02:10 +0900

    refactor: yml설정 후 테스트 코드 주석 해제

--- src/test/java/admin/adminsiteserver/AdminSiteServerApplicationTests.java
+++ src/test/java/admin/adminsiteserver/AdminSiteServerApplicationTests.java
@@ -2,12 +2,13 @@ package admin.adminsiteserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class AdminSiteServerApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
