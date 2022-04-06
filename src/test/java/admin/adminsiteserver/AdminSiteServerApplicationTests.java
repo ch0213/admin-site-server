6f2028749be0068d44d525f1862e5c3e052d6172
chkim <cndghks0213@gmail.com>
2022-04-07 00:21:26 +0900

    chore: 테스트 서버 배포를 위해 일시적으로 테스트 주석처리

--- src/test/java/admin/adminsiteserver/AdminSiteServerApplicationTests.java
+++ src/test/java/admin/adminsiteserver/AdminSiteServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AdminSiteServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
