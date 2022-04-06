0579ec17b46aadce7e84835fe1f495786491b056
chkim <cndghks0213@gmail.com>
2022-04-06 17:38:49 +0900

    feat: 답변 수정 메시지 추가

--- src/main/java/admin/adminsiteserver/qna/ui/QnaResponseMessage.java
+++ src/main/java/admin/adminsiteserver/qna/ui/QnaResponseMessage.java
@@ -7,7 +7,8 @@ public enum QnaResponseMessage {
     QNA_UPLOAD_SUCCESS("질문 등록 성공"),
     QNA_UPDATE_SUCCESS("질문 수정 성공"),
     QNA_DELETE_SUCCESS("질문 삭제 성공"),
-    ANSWER_UPLOAD_SUCCESS("답변 등록 성공");
+    ANSWER_UPLOAD_SUCCESS("답변 등록 성공"),
+    ANSWER_UPDATE_SUCCESS("답변 수정 성공");
 
     private final String message;
 
