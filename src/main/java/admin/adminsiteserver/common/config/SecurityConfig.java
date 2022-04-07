cd7eb5de04367a52f7ba1727710fe052b140248c
chkim <cndghks0213@gmail.com>
2022-04-07 13:16:31 +0900

    feat: 공지사항에 대한 조회 요청만 허용

--- src/main/java/admin/adminsiteserver/common/config/SecurityConfig.java
+++ src/main/java/admin/adminsiteserver/common/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import admin.adminsiteserver.member.auth.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
@@ -34,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/login", "/signup").permitAll()
+                .antMatchers(GET, "/announcement/**").permitAll()
                 .anyRequest().authenticated();
     }
 }
