5173022af407386e1ce98af7d49399a825254999
chkim <cndghks0213@gmail.com>
2022-04-07 15:57:19 +0900

    feat: Q&A 조회 메서드 추가

--- src/main/java/admin/adminsiteserver/qna/domain/QnaRepository.java
+++ src/main/java/admin/adminsiteserver/qna/domain/QnaRepository.java
@@ -1,6 +1,12 @@
 package admin.adminsiteserver.qna.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface QnaRepository extends JpaRepository<Qna, Long> {
+    Optional<Qna> findById(Long id);
+    Page<Qna> findAll(Pageable pageable);
 }
