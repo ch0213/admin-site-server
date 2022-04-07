27372b1df91ac4ae33645a35d8b902376684cfd4
chkim <cndghks0213@gmail.com>
2022-04-07 12:35:32 +0900

    refactor: image가 없을경우 json으로 변환하지 않도록 변경

--- src/main/java/admin/adminsiteserver/member/member/application/dto/MemberDto.java
+++ src/main/java/admin/adminsiteserver/member/member/application/dto/MemberDto.java
@@ -2,11 +2,15 @@ package admin.adminsiteserver.member.member.application.dto;
 
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import admin.adminsiteserver.member.member.domain.Member;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Getter
 @AllArgsConstructor
+@JsonInclude(NON_NULL)
 public class MemberDto {
 
     private Long id;
@@ -16,7 +20,7 @@ public class MemberDto {
     private String studentNumber;
     private String phoneNumber;
     private String role;
-    private FilePathDto filePathDto;
+    private FilePathDto profileImage;
 
     public static MemberDto of(Member member, FilePathDto filePathDto) {
         return new MemberDto(
