c787233a2dc0e12b1a2262a9df120af2fb29a77f
chkim <cndghks0213@gmail.com>
2022-04-07 15:56:39 +0900

    feat: 조회 성공 메시지 추가

--- src/main/java/admin/adminsiteserver/qna/ui/QnaResponseMessage.java
+++ src/main/java/admin/adminsiteserver/qna/ui/QnaResponseMessage.java
@@ -9,7 +9,9 @@ public enum QnaResponseMessage {
     QNA_DELETE_SUCCESS("질문 삭제 성공"),
     ANSWER_UPLOAD_SUCCESS("답변 등록 성공"),
     ANSWER_UPDATE_SUCCESS("답변 수정 성공"),
-    ANSWER_DELETE_SUCCESS("답변 삭제 성공");
+    ANSWER_DELETE_SUCCESS("답변 삭제 성공"),
+    INQUIRE_QNA_SUCCESS("질문 조회 성공"),
+    INQUIRE_QNA_LIST_SUCCESS("질문 목록 조회 성공");
 
     private final String message;
 
