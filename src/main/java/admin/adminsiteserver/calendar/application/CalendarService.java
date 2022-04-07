1e255fc4f439868112b40b51a394dcf1e0584b6f
chkim <cndghks0213@gmail.com>
2022-04-08 02:36:03 +0900

    feat: 일정 수정 기능 구현

--- src/main/java/admin/adminsiteserver/calendar/application/CalendarService.java
+++ src/main/java/admin/adminsiteserver/calendar/application/CalendarService.java
@@ -1,7 +1,9 @@
 package admin.adminsiteserver.calendar.application;
 
 import admin.adminsiteserver.calendar.application.dto.CalendarResponse;
+import admin.adminsiteserver.calendar.domain.Calendar;
 import admin.adminsiteserver.calendar.domain.CalendarRepository;
+import admin.adminsiteserver.calendar.exception.NotExistCalendarException;
 import admin.adminsiteserver.calendar.ui.dto.CalendarRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,5 +19,13 @@ public class CalendarService {
     @Transactional
     public CalendarResponse uploadCalendar(CalendarRequest calendarRequest) {
         return CalendarResponse.from(calendarRepository.save(calendarRequest.toCalendar()));
+    }
+
+    @Transactional
+    public CalendarResponse updateCalendar(CalendarRequest calendarRequest, Long calendarId) {
+        Calendar calendar = calendarRepository.findById(calendarId)
+                .orElseThrow(NotExistCalendarException::new);
+        calendar.updateTitleAndStartDate(calendarRequest.getTitle(), calendar.getStartDate());
+        return CalendarResponse.from(calendar);
     }
 }
