448cc71831babe83d877196e94331c3c25fc007a
chkim <cndghks0213@gmail.com>
2022-04-07 21:58:27 +0900

    fix: Origin -> OriginPattern

--- src/main/java/admin/adminsiteserver/common/config/SecurityConfig.java
+++ src/main/java/admin/adminsiteserver/common/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
