19e916940783d88790f564ec88557187c8a77c3c
chkim <cndghks0213@gmail.com>
2022-04-08 23:32:09 +0900

    feat: 권한 신청 변경 성공 메시지 추가

--- src/main/java/admin/adminsiteserver/levelup/ui/LevelUpResponseMessage.java
+++ src/main/java/admin/adminsiteserver/levelup/ui/LevelUpResponseMessage.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum LevelUpResponseMessage {
-    LEVEL_UP_REGISTER_SUCCESS("권한 업그레이드 신청 성공");
+    LEVEL_UP_REGISTER_SUCCESS("권한 업그레이드 신청 성공"),
+    LEVEL_UP_UPDATE_SUCCESS("권한 업그레이드 수정 성공");
 
     private final String message;
 }
