4803d645a759952977af8a79471bab580ad20c80
chkim <cndghks0213@gmail.com>
2022-04-08 20:25:57 +0900

    feat: 권한 업그레이드 요청, 응답 dto 생성

---
+++ src/main/java/admin/adminsiteserver/levelup/application/dto/LevelUpResponse.java
@@ -0,0 +1,24 @@
+package admin.adminsiteserver.levelup.application.dto;
+
+import admin.adminsiteserver.levelup.domain.LevelUp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class LevelUpResponse {
+
+    private String name;
+    private String registerRoleType;
+    private LocalDateTime registerAt;
+
+    public static LevelUpResponse from(LevelUp levelUp) {
+        return new LevelUpResponse(
+                levelUp.getMember().getName(),
+                levelUp.getRole().getDescription(),
+                levelUp.getCreatedAt()
+        );
+    }
+}
---
+++ src/main/java/admin/adminsiteserver/levelup/ui/dto/LevelUpRequest.java
@@ -0,0 +1,29 @@
+package admin.adminsiteserver.levelup.ui.dto;
+
+import admin.adminsiteserver.levelup.domain.LevelUp;
+import admin.adminsiteserver.levelup.exception.NotExistRoleException;
+import admin.adminsiteserver.member.member.domain.Member;
+import admin.adminsiteserver.member.member.domain.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LevelUpRequest {
+    private String role;
+
+    public LevelUp from(Member member) {
+        return new LevelUp(findRoleType(), false, member);
+    }
+
+    private RoleType findRoleType() {
+        return Arrays.stream(RoleType.values()).sequential()
+                .filter(roleType -> roleType.getDescription().equals(role))
+                .findAny()
+                .orElseThrow(NotExistRoleException::new);
+    }
+}
