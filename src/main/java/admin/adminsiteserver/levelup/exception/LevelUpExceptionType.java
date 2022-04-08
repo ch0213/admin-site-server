fb3bf1ca2dad179305b4a73126d3a846342810a6
chkim <cndghks0213@gmail.com>
2022-04-08 23:31:07 +0900

    feat: 존재하지 않는 신청일 경우 발생하는 예외 추가

--- src/main/java/admin/adminsiteserver/levelup/exception/LevelUpExceptionType.java
+++ src/main/java/admin/adminsiteserver/levelup/exception/LevelUpExceptionType.java
@@ -9,7 +9,8 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @RequiredArgsConstructor
 public enum LevelUpExceptionType {
-    NOT_EXIST_ROLE("존재하지 않는 역할입니다.", BAD_REQUEST);
+    NOT_EXIST_ROLE("존재하지 않는 역할입니다.", BAD_REQUEST),
+    NOT_EXIST_LEVELUP("존재하지 신청입니다.", BAD_REQUEST);
 
     private final String message;
     private final HttpStatus status;
