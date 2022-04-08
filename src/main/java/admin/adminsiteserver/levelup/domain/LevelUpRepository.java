07af7018a8fa8f25d6aa270994a9ec66f587d31a
chkim <cndghks0213@gmail.com>
2022-04-08 20:26:55 +0900

    feat: 권한 업그레이드 신청 레포지토리 생성

---
+++ src/main/java/admin/adminsiteserver/levelup/domain/LevelUpRepository.java
@@ -0,0 +1,6 @@
+package admin.adminsiteserver.levelup.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LevelUpRepository extends JpaRepository<LevelUp, Long> {
+}
