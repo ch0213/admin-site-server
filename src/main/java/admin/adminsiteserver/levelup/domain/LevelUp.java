9a390646e47710425ca92a39bb4b4762c5de896b
chkim <cndghks0213@gmail.com>
2022-04-08 23:31:48 +0900

    refactor: role을 RoleType대신 String으로 변경

--- src/main/java/admin/adminsiteserver/levelup/domain/LevelUp.java
+++ src/main/java/admin/adminsiteserver/levelup/domain/LevelUp.java
@@ -1,15 +1,16 @@
 package admin.adminsiteserver.levelup.domain;
 
 import admin.adminsiteserver.common.domain.BaseTimeEntity;
+import admin.adminsiteserver.levelup.exception.NotExistRoleException;
 import admin.adminsiteserver.member.member.domain.Member;
 import admin.adminsiteserver.member.member.domain.RoleType;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-import static javax.persistence.EnumType.STRING;
+import java.util.Arrays;
+
 import static lombok.AccessLevel.*;
 
 @Entity
@@ -20,8 +21,7 @@ public class LevelUp extends BaseTimeEntity {
     @Id @GeneratedValue
     private Long id;
 
-    @Enumerated(STRING)
-    private RoleType role;
+    private String role;
 
     private boolean processed;
 
@@ -29,9 +29,17 @@ public class LevelUp extends BaseTimeEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    public LevelUp(RoleType role, boolean processed, Member member) {
+    public LevelUp(String role, boolean processed, Member member) {
         this.role = role;
         this.processed = processed;
         this.member = member;
+    }
+
+    public void updateRole(String newRole) {
+        this.role = Arrays.stream(RoleType.values()).sequential()
+                .filter(roleType -> roleType.getDescription().equals(newRole))
+                .findAny()
+                .orElseThrow(NotExistRoleException::new)
+                .getDescription();
     }
 }
