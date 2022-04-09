84fb5e3b075b3ffd12cd46513c90dd448ccb5619
chkim <cndghks0213@gmail.com>
2022-04-09 18:02:01 +0900

    feat: 갤러리 관련 요청 형태 정의

---
+++ src/main/java/admin/adminsiteserver/gallery/ui/dto/BaseGalleryRequest.java
@@ -0,0 +1,15 @@
+package admin.adminsiteserver.gallery.ui.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public abstract class BaseGalleryRequest {
+    private String title;
+    private String content;
+    private List<MultipartFile> images;
+}
---
+++ src/main/java/admin/adminsiteserver/gallery/ui/dto/UpdateGalleryRequest.java
@@ -0,0 +1,16 @@
+package admin.adminsiteserver.gallery.ui.dto;
+
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+public class UpdateGalleryRequest extends BaseGalleryRequest {
+    private List<String> deleteFileUrls;
+
+    public UpdateGalleryRequest(String title, String content, List<MultipartFile> images, List<String> deleteFileUrls) {
+        super(title, content, images);
+        this.deleteFileUrls = deleteFileUrls;
+    }
+}
