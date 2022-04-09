2ed83b3309313af0f7e5c59a73c583a818037a06
chkim <cndghks0213@gmail.com>
2022-04-09 15:05:37 +0900

    feat: 응답 메시지 추가

--- src/main/java/admin/adminsiteserver/levelup/ui/LevelUpResponseMessage.java
+++ src/main/java/admin/adminsiteserver/levelup/ui/LevelUpResponseMessage.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum LevelUpResponseMessage {
     LEVEL_UP_REGISTER_SUCCESS("권한 업그레이드 신청 성공"),
     LEVEL_UP_UPDATE_SUCCESS("권한 업그레이드 수정 성공"),
-    LEVEL_UP_DELETE_SUCCESS("권한 업그레이드 취소 성공");
+    LEVEL_UP_DELETE_SUCCESS("권한 업그레이드 취소 성공"),
+    LEVEL_UP_INQUIRE_SUCCESS("권한 업그레이드 신청 조회 성공"),
+    LEVEL_UP_APPROVE_SUCCESS("권한 업그레이드 신청 승인"),
+    LEVEL_UP_REJECT_SUCCESS("권한 업그레이드 신청 거절");
 
     private final String message;
 }
