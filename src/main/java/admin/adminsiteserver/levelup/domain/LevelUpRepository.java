d6cb6fc5e84e77b1a15ae9bbf603f0982b05f2cf
chkim <cndghks0213@gmail.com>
2022-04-09 15:06:06 +0900

    feat: 다양한 조건을 기준으로 LevelUp을 찾는 메서드 추가

--- src/main/java/admin/adminsiteserver/levelup/domain/LevelUpRepository.java
+++ src/main/java/admin/adminsiteserver/levelup/domain/LevelUpRepository.java
@@ -2,5 +2,11 @@ package admin.adminsiteserver.levelup.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LevelUpRepository extends JpaRepository<LevelUp, Long> {
+    List<LevelUp> findByProcessedIsFalse();
+    Optional<LevelUp> findByUserId(String userId);
+    List<LevelUp> findAllById(Iterable<Long> longs);
 }
