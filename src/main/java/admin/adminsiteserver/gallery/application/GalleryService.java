b04e9fcf1510698719eae95dc822c7df35ecdf58
chkim <cndghks0213@gmail.com>
2022-04-09 18:04:06 +0900

    feat: 갤러리 관련 기능(추가/수정/삭제/조회) 구현

---
+++ src/main/java/admin/adminsiteserver/gallery/application/GalleryService.java
@@ -0,0 +1,129 @@
+package admin.adminsiteserver.gallery.application;
+
+import admin.adminsiteserver.common.aws.infrastructure.S3Uploader;
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
+import admin.adminsiteserver.common.dto.CommonResponse;
+import admin.adminsiteserver.common.dto.PageInfo;
+import admin.adminsiteserver.gallery.application.dto.GalleryResponse;
+import admin.adminsiteserver.gallery.domain.Gallery;
+import admin.adminsiteserver.gallery.domain.GalleryFilePath;
+import admin.adminsiteserver.gallery.domain.GalleryFilePathRepository;
+import admin.adminsiteserver.gallery.domain.GalleryRepository;
+import admin.adminsiteserver.gallery.exception.NotExistGalleryException;
+import admin.adminsiteserver.gallery.exception.UnauthorizedForGalleryException;
+import admin.adminsiteserver.gallery.ui.dto.BaseGalleryRequest;
+import admin.adminsiteserver.gallery.ui.dto.UpdateGalleryRequest;
+import admin.adminsiteserver.gallery.ui.dto.UploadGalleryRequest;
+import admin.adminsiteserver.member.auth.util.dto.LoginUserInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static admin.adminsiteserver.gallery.ui.GalleryResponseMessage.GALLERY_FIND_ALL_SUCCESS;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class GalleryService {
+
+    private final GalleryRepository galleryRepository;
+    private final GalleryFilePathRepository filePathRepository;
+    private final S3Uploader s3Uploader;
+    private static final String GALLERY_IMAGE_PATH = "gallery/";
+
+    @Transactional
+    public GalleryResponse upload(UploadGalleryRequest request, LoginUserInfo loginUserInfo) {
+        Gallery gallery = request.createGallery(loginUserInfo);
+        List<FilePathDto> filePathDtos = new ArrayList<>();
+        if (request.getImages() != null) {
+            filePathDtos = saveFiles(request);
+            List<GalleryFilePath> imagePaths = getImagePathsFromDto(filePathDtos);
+            gallery.addImages(imagePaths);
+        }
+        galleryRepository.save(gallery);
+        return GalleryResponse.of(gallery, filePathDtos);
+    }
+
+    @Transactional
+    public GalleryResponse update(UpdateGalleryRequest request, LoginUserInfo loginUserInfo, Long id) {
+        Gallery gallery = galleryRepository.findById(id)
+                .orElseThrow(NotExistGalleryException::new);
+        validateAuthorityForGallery(loginUserInfo, gallery);
+        gallery.updateTitleAndContent(request.getTitle(), request.getContent());
+
+        if (request.getImages() != null) {
+            List<FilePathDto> filePathDtos = saveFiles(request);
+            List<GalleryFilePath> filePaths = filePathRepository.saveAll(getImagePathsFromDto(filePathDtos));
+            gallery.addImages(filePaths);
+        }
+
+        if (request.getDeleteFileUrls() != null) {
+            gallery.deleteImages(request.getDeleteFileUrls());
+            s3Uploader.delete(request.getDeleteFileUrls());
+        }
+
+        return GalleryResponse.of(gallery, getImagePathDtosFromGallery(gallery));
+    }
+
+    @Transactional
+    public void delete(Long galleryId, LoginUserInfo loginUserInfo) {
+        Gallery gallery = galleryRepository.findById(galleryId)
+                .orElseThrow(NotExistGalleryException::new);
+        validateAuthorityForGallery(loginUserInfo, gallery);
+        List<String> deleteFileURls = gallery.getImages().stream()
+                .map(GalleryFilePath::getFileUrl)
+                .collect(Collectors.toList());
+        s3Uploader.delete(deleteFileURls);
+
+        galleryRepository.delete(gallery);
+    }
+
+    private void validateAuthorityForGallery(LoginUserInfo loginUserInfo, Gallery gallery) {
+        if (!loginUserInfo.getUserId().equals(gallery.getAuthorId())) {
+            throw new UnauthorizedForGalleryException();
+        }
+    }
+
+    public GalleryResponse find(Long galleryId) {
+        Gallery gallery = galleryRepository.findById(galleryId)
+                .orElseThrow(NotExistGalleryException::new);
+        return GalleryResponse.from(gallery);
+    }
+
+    public CommonResponse<List<GalleryResponse>> findAll(Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("createdAt").descending());
+        Page<GalleryResponse> gallerys = galleryRepository.findAll(pageRequest)
+                .map(GalleryResponse::from);
+
+        return CommonResponse.of(gallerys.getContent(), PageInfo.from(gallerys), GALLERY_FIND_ALL_SUCCESS.getMessage());
+    }
+
+    private List<FilePathDto> getImagePathDtosFromGallery(Gallery gallery) {
+        return gallery.getImages().stream()
+                .map(FilePathDto::from)
+                .collect(Collectors.toList());
+    }
+
+    private List<FilePathDto> saveFiles(BaseGalleryRequest request) {
+        if (request.getImages() == null) {
+            return null;
+        }
+        return s3Uploader.upload(request.getImages(), GALLERY_IMAGE_PATH);
+    }
+
+    private List<GalleryFilePath> getImagePathsFromDto(List<FilePathDto> imagePathDtos) {
+        return imagePathDtos.stream()
+                .map(filePathDto -> filePathDto.toFilePath(GalleryFilePath.class))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/admin/adminsiteserver/gallery/ui/GalleryController.java
@@ -0,0 +1,58 @@
+package admin.adminsiteserver.gallery.ui;
+
+import admin.adminsiteserver.common.dto.CommonResponse;
+import admin.adminsiteserver.gallery.application.GalleryService;
+import admin.adminsiteserver.gallery.application.dto.GalleryResponse;
+import admin.adminsiteserver.gallery.ui.dto.UpdateGalleryRequest;
+import admin.adminsiteserver.gallery.ui.dto.UploadGalleryRequest;
+import admin.adminsiteserver.member.auth.util.LoginUser;
+import admin.adminsiteserver.member.auth.util.dto.LoginUserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static admin.adminsiteserver.gallery.ui.GalleryResponseMessage.*;
+
+@RestController
+@RequestMapping("/gallery")
+@RequiredArgsConstructor
+public class GalleryController {
+
+    private final GalleryService galleryService;
+
+    @PostMapping
+    public CommonResponse<GalleryResponse> uploadGallery(UploadGalleryRequest request,
+                                                         @LoginUser LoginUserInfo loginUserInfo) {
+        GalleryResponse response = galleryService.upload(request, loginUserInfo);
+        return CommonResponse.of(response, GALLERY_UPLOAD_SUCCESS.getMessage());
+    }
+
+    @PutMapping("/{galleryId}")
+    public CommonResponse<GalleryResponse> updateGallery(
+            UpdateGalleryRequest request,
+            @LoginUser LoginUserInfo loginUserInfo,
+            @PathVariable Long galleryId
+    )
+    {
+        GalleryResponse galleryResponse = galleryService.update(request, loginUserInfo, galleryId);
+        return CommonResponse.of(galleryResponse, GALLERY_UPDATE_SUCCESS.getMessage());
+    }
+
+    @DeleteMapping("/{galleryId}")
+    public CommonResponse<Void> deleteGallery(@LoginUser LoginUserInfo loginUserInfo, @PathVariable Long galleryId) {
+        galleryService.delete(galleryId, loginUserInfo);
+        return CommonResponse.from(GALLERY_DELETE_SUCCESS.getMessage());
+    }
+
+    @GetMapping("/{galleryId}")
+    public CommonResponse<GalleryResponse> findGallery(@PathVariable Long galleryId) {
+        return CommonResponse.of(galleryService.find(galleryId), GALLERY_FIND_SUCCESS.getMessage());
+    }
+
+    @GetMapping
+    public CommonResponse<List<GalleryResponse>> findAllGallery(Pageable pageable) {
+        return galleryService.findAll(pageable);
+    }
+}
