81e3b7acbcc7723a6385f87f9146cbf1237bcd21
chkim <cndghks0213@gmail.com>
2022-04-11 12:02:40 +0900

    feat: 갤러리 관련 도메인 생성

--- src/main/java/admin/adminsiteserver/gallery/domain/Gallery.java
+++ src/main/java/admin/adminsiteserver/gallery/domain/Gallery.java
@@ -1,5 +1,6 @@
 package admin.adminsiteserver.gallery.domain;
 
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import admin.adminsiteserver.common.domain.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static javax.persistence.CascadeType.ALL;
 import static lombok.AccessLevel.PROTECTED;
@@ -31,15 +33,17 @@ public class Gallery extends BaseTimeEntity {
     @Lob
     private String content;
 
-    @OneToMany(mappedBy = "gallery", cascade = ALL, orphanRemoval = true)
+    @OneToMany(cascade = ALL, orphanRemoval = true)
+    @JoinColumn(name = "gallery_id")
     private List<GalleryFilePath> files = new ArrayList<>();
 
     @Builder
-    public Gallery(String authorId, String authorName, String title, String content) {
+    public Gallery(String authorId, String authorName, String title, String content, List<GalleryFilePath> files) {
         this.authorId = authorId;
         this.authorName = authorName;
         this.title = title;
         this.content = content;
+        this.files = files;
     }
 
     public void updateTitleAndContent(String title, String content) {
@@ -47,21 +51,9 @@ public class Gallery extends BaseTimeEntity {
         this.content = content;
     }
 
-    public void addFiles(List<GalleryFilePath> newFilePaths) {
-        if (newFilePaths == null) {
-            return;
-        }
-
-        for (GalleryFilePath newFilePath : newFilePaths) {
-            newFilePath.includedToGallery(this);
-            this.files.add(newFilePath);
-        }
-    }
-
-    public void deleteFiles(List<String> deleteFileUrls) {
-        if (deleteFileUrls == null) {
-            return;
-        }
-        files.removeIf(filePath -> deleteFileUrls.contains(filePath.getFileUrl()));
+    public void deleteFiles(List<FilePathDto> deleteFileUrls) {
+        files.removeIf(filePath -> deleteFileUrls.stream().map(FilePathDto::getFileUrl)
+                .collect(Collectors.toList())
+                .contains(filePath.getFileUrl()));
     }
 }
