d6baabed9ae793453303446642efb0a07c55e65b
chkim <cndghks0213@gmail.com>
2022-04-11 11:57:14 +0900

    refactor: 공지사항 관련 폼 수정

--- src/main/java/admin/adminsiteserver/announcement/ui/dto/UpdateAnnouncementRequest.java
+++ src/main/java/admin/adminsiteserver/announcement/ui/dto/UpdateAnnouncementRequest.java
@@ -1,15 +1,18 @@
 package admin.adminsiteserver.announcement.ui.dto;
 
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class UpdateAnnouncementRequest extends BaseAnnouncementRequest{
-    private List<String> deleteFileUrls;
+    private List<FilePathDto> deleteFileUrls;
 
-    public UpdateAnnouncementRequest(String title, String content, List<MultipartFile> files, List<String> deleteFileUrls) {
+    public UpdateAnnouncementRequest(String title, String content, List<FilePathDto> files, List<FilePathDto> deleteFileUrls) {
         super(title, content, files);
         this.deleteFileUrls = deleteFileUrls;
     }
--- src/main/java/admin/adminsiteserver/announcement/ui/dto/UploadAnnouncementRequest.java
+++ src/main/java/admin/adminsiteserver/announcement/ui/dto/UploadAnnouncementRequest.java
@@ -1,24 +1,37 @@
 package admin.adminsiteserver.announcement.ui.dto;
 
 import admin.adminsiteserver.announcement.domain.Announcement;
+import admin.adminsiteserver.announcement.domain.AnnouncementFilePath;
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import admin.adminsiteserver.member.auth.util.dto.LoginUserInfo;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
+@NoArgsConstructor
 public class UploadAnnouncementRequest extends BaseAnnouncementRequest{
-    public UploadAnnouncementRequest(String title, String content, List<MultipartFile> files) {
+    public UploadAnnouncementRequest(String title, String content, List<FilePathDto> files) {
         super(title, content, files);
     }
 
     public Announcement createAnnouncement(LoginUserInfo loginUserInfo) {
+        List<AnnouncementFilePath> filePaths = new ArrayList<>();
+        if (getFiles() != null) {
+            filePaths = getFiles().stream()
+                    .map(filePathDto -> filePathDto.toFilePath(AnnouncementFilePath.class))
+                    .collect(Collectors.toList());
+        }
+
         return Announcement.builder()
                 .authorId(loginUserInfo.getUserId())
                 .authorName(loginUserInfo.getName())
                 .title(getTitle())
                 .content(getContent())
+                .files(filePaths)
                 .build();
     }
 }
