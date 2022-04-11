abb6453cf7c0b91041f99440624984d66388e8b2
chkim <cndghks0213@gmail.com>
2022-04-12 00:45:26 +0900

    feat: file 삭제 기능 구현

--- src/main/java/admin/adminsiteserver/common/aws/appliaction/FileUploadService.java
+++ src/main/java/admin/adminsiteserver/common/aws/appliaction/FileUploadService.java
@@ -2,9 +2,12 @@ package admin.adminsiteserver.common.aws.appliaction;
 
 import admin.adminsiteserver.common.aws.infrastructure.S3Uploader;
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
+import admin.adminsiteserver.common.aws.ui.dto.FileDeleteRequest;
 import admin.adminsiteserver.common.aws.ui.dto.FileUploadRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,11 @@ public class FileUploadService {
     private final S3Uploader s3Uploader;
     private static final String FILE_STORAGE_PATH = "file/";
 
-    public FilePathDto uploadImage(FileUploadRequest request) {
-        return s3Uploader.upload(request.getFile(), FILE_STORAGE_PATH);
+    public List<FilePathDto> uploadFile(FileUploadRequest request) {
+        return s3Uploader.upload(request.getFiles(), FILE_STORAGE_PATH);
+    }
+
+    public void deleteFile(FileDeleteRequest request) {
+        s3Uploader.delete(request.getDeleteFileUrls());
     }
 }
--- src/main/java/admin/adminsiteserver/common/aws/ui/FileUploadController.java
+++ src/main/java/admin/adminsiteserver/common/aws/ui/FileUploadController.java
@@ -2,12 +2,13 @@ package admin.adminsiteserver.common.aws.ui;
 
 import admin.adminsiteserver.common.aws.appliaction.FileUploadService;
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
+import admin.adminsiteserver.common.aws.ui.dto.FileDeleteRequest;
 import admin.adminsiteserver.common.aws.ui.dto.FileUploadRequest;
 import admin.adminsiteserver.common.dto.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/file")
@@ -16,10 +17,17 @@ public class FileUploadController {
 
     private final FileUploadService fileUploadService;
     private static final String FILE_UPLOAD_MESSAGE = "파일 업로드 성공";
+    private static final String FILE_DELETE_MESSAGE = "파일 삭제 성공";
 
     @PostMapping
-    public CommonResponse<FilePathDto> uploadFile(FileUploadRequest request) {
-        FilePathDto filePathDto = fileUploadService.uploadImage(request);
-        return CommonResponse.of(filePathDto, FILE_UPLOAD_MESSAGE);
+    public CommonResponse<List<FilePathDto>> uploadFile(FileUploadRequest request) {
+        List<FilePathDto> filePathDtos = fileUploadService.uploadFile(request);
+        return CommonResponse.of(filePathDtos, FILE_UPLOAD_MESSAGE);
+    }
+
+    @DeleteMapping
+    public CommonResponse<Void> deleteFile(@RequestBody FileDeleteRequest request) {
+        fileUploadService.deleteFile(request);
+        return CommonResponse.from(FILE_DELETE_MESSAGE);
     }
 }
