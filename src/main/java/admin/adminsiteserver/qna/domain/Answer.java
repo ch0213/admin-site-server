edcd749bf2c6e118a2134810c0550131f2ceddb1
chkim <cndghks0213@gmail.com>
2022-04-11 12:00:36 +0900

    feat: 파일 삭제 기능 구현

--- src/main/java/admin/adminsiteserver/qna/domain/Answer.java
+++ src/main/java/admin/adminsiteserver/qna/domain/Answer.java
@@ -1,5 +1,6 @@
 package admin.adminsiteserver.qna.domain;
 
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import admin.adminsiteserver.common.domain.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import javax.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static javax.persistence.CascadeType.ALL;
 import static lombok.AccessLevel.*;
@@ -31,12 +33,13 @@ public class Answer extends BaseTimeEntity {
 
     @OneToMany(cascade = ALL, orphanRemoval = true)
     @JoinColumn(name = "answer_id")
-    private List<AnswerFilePath> images = new ArrayList<>();
+    private List<AnswerFilePath> files = new ArrayList<>();
 
-    public Answer(String authorId, String authorName, String content) {
+    public Answer(String authorId, String authorName, String content, List<AnswerFilePath> files) {
         this.authorId = authorId;
         this.authorName = authorName;
         this.content = content;
+        this.files = files;
     }
 
     public void updateContent(String content) {
@@ -47,13 +50,12 @@ public class Answer extends BaseTimeEntity {
         if (images == null) {
             return;
         }
-        this.images.addAll(images);
+        this.files.addAll(images);
     }
 
-    public void deleteImages(List<String> deleteFileUrls) {
-        if (deleteFileUrls == null) {
-            return;
-        }
-        images.removeIf(filePath -> deleteFileUrls.contains(filePath.getFileUrl()));
+    public void deleteFiles(List<FilePathDto> deleteFileUrls) {
+        files.removeIf(filePath -> deleteFileUrls.stream().map(FilePathDto::getFileUrl)
+                .collect(Collectors.toList())
+                .contains(filePath.getFileUrl()));
     }
 }
--- src/main/java/admin/adminsiteserver/qna/domain/Qna.java
+++ src/main/java/admin/adminsiteserver/qna/domain/Qna.java
@@ -1,5 +1,6 @@
 package admin.adminsiteserver.qna.domain;
 
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import admin.adminsiteserver.common.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static javax.persistence.CascadeType.*;
 import static lombok.AccessLevel.*;
@@ -30,19 +32,20 @@ public class Qna extends BaseTimeEntity {
 
     @OneToMany(cascade = ALL, orphanRemoval = true)
     @JoinColumn(name = "qna_id")
-    private List<QuestionFilePath> images = new ArrayList<>();
+    private List<QuestionFilePath> files = new ArrayList<>();
 
     @OneToMany(cascade = ALL, orphanRemoval = true)
     @JoinColumn(name = "qna_id")
     private List<Answer> answers = new ArrayList<>();
 
     @Builder
-    public Qna(Long id, String authorId, String authorName, String title, String content) {
+    public Qna(Long id, String authorId, String authorName, String title, String content, List<QuestionFilePath> files) {
         this.id = id;
         this.authorId = authorId;
         this.authorName = authorName;
         this.title = title;
         this.content = content;
+        this.files = files;
     }
 
     public void updateContentAndTitle(String title, String content) {
@@ -50,21 +53,13 @@ public class Qna extends BaseTimeEntity {
         this.content = content;
     }
 
-    public void addQuestionImages(List<QuestionFilePath> newFilePaths) {
-        if (newFilePaths == null) {
-            return;
-        }
-        this.images.addAll(newFilePaths);
-    }
-
     public void addAnswer(Answer answer) {
         this.answers.add(answer);
     }
 
-    public void deleteImages(List<String> deleteFileUrls) {
-        if (deleteFileUrls == null) {
-            return;
-        }
-        images.removeIf(filePath -> deleteFileUrls.contains(filePath.getFileUrl()));
+    public void deleteFiles(List<FilePathDto> deleteFileUrls) {
+        files.removeIf(filePath -> deleteFileUrls.stream().map(FilePathDto::getFileUrl)
+                .collect(Collectors.toList())
+                .contains(filePath.getFileUrl()));
     }
 }
