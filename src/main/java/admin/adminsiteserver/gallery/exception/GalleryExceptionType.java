326bbb9ecdf52467084b13c3ac3e190133316325
chkim <cndghks0213@gmail.com>
2022-04-11 12:03:28 +0900

    feat: 갤러리 관련 예외 정의

--- src/main/java/admin/adminsiteserver/gallery/exception/GalleryExceptionType.java
+++ src/main/java/admin/adminsiteserver/gallery/exception/GalleryExceptionType.java
@@ -10,8 +10,8 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 @Getter
 @AllArgsConstructor
 public enum GalleryExceptionType {
-    NOT_EXIST_GALLERY("존재하지 않는 갤러리입니다.", BAD_REQUEST),
-    UNAUTHORIZED_FOR_GALLERY("갤러리에 대한 권한이 없습니다.", UNAUTHORIZED);
+    NOT_EXIST_GALLERY("존재하지 않는 공지사항입니다.", BAD_REQUEST),
+    UNAUTHORIZED_FOR_GALLERY("공지사항에 대한 권한이 없습니다.", UNAUTHORIZED);
 
     private final String message;
     private HttpStatus status;
