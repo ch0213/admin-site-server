c6f2f26db8391e71d0771ba8b302f0421a6186a5
chkim <cndghks0213@gmail.com>
2022-04-11 11:53:34 +0900

    refactor: 이미지 업로드 방식 변경에 따른 Announcement 변경

--- src/main/java/admin/adminsiteserver/announcement/domain/Announcement.java
+++ src/main/java/admin/adminsiteserver/announcement/domain/Announcement.java
@@ -1,5 +1,6 @@
 package admin.adminsiteserver.announcement.domain;
 
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import admin.adminsiteserver.common.domain.BaseTimeEntity;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static javax.persistence.CascadeType.*;
 import static lombok.AccessLevel.*;
@@ -28,15 +30,17 @@ public class Announcement extends BaseTimeEntity {
     @Lob
     private String content;
 
-    @OneToMany(mappedBy = "announcement", cascade = ALL, orphanRemoval = true)
+    @OneToMany(cascade = ALL, orphanRemoval = true)
+    @JoinColumn(name = "announcement_id")
     private List<AnnouncementFilePath> files = new ArrayList<>();
 
     @Builder
-    public Announcement(String authorId, String authorName, String title, String content) {
+    public Announcement(String authorId, String authorName, String title, String content, List<AnnouncementFilePath> files) {
         this.authorId = authorId;
         this.authorName = authorName;
         this.title = title;
         this.content = content;
+        this.files = files;
     }
 
     public void updateTitleAndContent(String title, String content) {
@@ -44,21 +48,9 @@ public class Announcement extends BaseTimeEntity {
         this.content = content;
     }
 
-    public void addFiles(List<AnnouncementFilePath> newFilePaths) {
-        if (newFilePaths == null) {
-            return;
-        }
-
-        for (AnnouncementFilePath newFilePath : newFilePaths) {
-            newFilePath.includedToAnnouncement(this);
-            this.files.add(newFilePath);
-        }
-    }
-
-    public void deleteFiles(List<String> deleteFileUrls) {
-        if (deleteFileUrls == null) {
-            return;
-        }
-        files.removeIf(filePath -> deleteFileUrls.contains(filePath.getFileUrl()));
+    public void deleteFiles(List<FilePathDto> deleteFileUrls) {
+        files.removeIf(filePath -> deleteFileUrls.stream().map(FilePathDto::getFileUrl)
+                .collect(Collectors.toList())
+                .contains(filePath.getFileUrl()));
     }
 }
