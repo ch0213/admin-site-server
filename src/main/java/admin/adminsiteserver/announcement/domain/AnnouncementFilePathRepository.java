aca4f4d0d294c940bca293c075bb995dca005dab
chkim <cndghks0213@gmail.com>
2022-04-14 01:14:54 +0900

    rafactor: 불필요한 repository 삭제

--- src/main/java/admin/adminsiteserver/announcement/domain/AnnouncementFilePathRepository.java
+++
@@ -1,6 +0,0 @@
-package admin.adminsiteserver.announcement.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnouncementFilePathRepository extends JpaRepository<AnnouncementFilePath, Long> {
-}
