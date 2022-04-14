4abd13aa48714b4e83132f03a73a90f1594b11d8
chkim <cndghks0213@gmail.com>
2022-04-14 16:28:40 +0900

    feat: GalleryComment 도메인 생성

---
+++ src/main/java/admin/adminsiteserver/gallery/domain/GalleryComment.java
@@ -0,0 +1,36 @@
+package admin.adminsiteserver.gallery.domain;
+
+import admin.adminsiteserver.common.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class GalleryComment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String authorId;
+    private String authorName;
+    private String comment;
+
+    public GalleryComment(String authorId, String authorName, String comment) {
+        this.authorId = authorId;
+        this.authorName = authorName;
+        this.comment = comment;
+    }
+
+    public void updateComment(String comment) {
+        this.comment = comment;
+    }
+}
