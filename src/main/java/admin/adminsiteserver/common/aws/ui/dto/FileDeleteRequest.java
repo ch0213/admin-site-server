c71a54ec483435a24540451c5abcc7672a928cca
chkim <cndghks0213@gmail.com>
2022-04-16 00:26:47 +0900

    fix: file upload, delete 요청시 발생 에러 해결

--- src/main/java/admin/adminsiteserver/common/aws/ui/dto/FileDeleteRequest.java
+++ src/main/java/admin/adminsiteserver/common/aws/ui/dto/FileDeleteRequest.java
@@ -3,10 +3,12 @@ package admin.adminsiteserver.common.aws.ui.dto;
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class FileDeleteRequest {
     private List<FilePathDto> deleteFileUrls;
--- src/main/java/admin/adminsiteserver/common/aws/ui/dto/FileUploadRequest.java
+++ src/main/java/admin/adminsiteserver/common/aws/ui/dto/FileUploadRequest.java
@@ -2,13 +2,11 @@ package admin.adminsiteserver.common.aws.ui.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class FileUploadRequest {
     private List<MultipartFile> files;
