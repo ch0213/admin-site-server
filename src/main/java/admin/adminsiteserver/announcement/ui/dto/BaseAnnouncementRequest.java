8dacff7205f4a30d6df63b4cc40ce2fdc6b350b5
chkim <cndghks0213@gmail.com>
2022-04-23 20:28:34 +0900

    feat: filePathDto -> AnnouncementFilePath로 변환하는 기능 추가

--- src/main/java/admin/adminsiteserver/announcement/ui/dto/BaseAnnouncementRequest.java
+++ src/main/java/admin/adminsiteserver/announcement/ui/dto/BaseAnnouncementRequest.java
@@ -1,5 +1,6 @@
 package admin.adminsiteserver.announcement.ui.dto;
 
+import admin.adminsiteserver.announcement.domain.AnnouncementFilePath;
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -15,4 +17,10 @@ public abstract class BaseAnnouncementRequest {
     private String title;
     private String content;
     private List<FilePathDto> files;
+
+    public List<AnnouncementFilePath> toAnnouncementFilePaths() {
+        return getFiles().stream()
+                .map(filePathDto -> filePathDto.toFilePath(AnnouncementFilePath.class))
+                .collect(Collectors.toList());
+    }
 }
