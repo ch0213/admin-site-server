ecee16e9bf03b055096fa87e6230ce6160819648
chkim <cndghks0213@gmail.com>
2022-04-26 05:59:12 +0900

    feat: 불필요한 정적 팩토리 메서드 제거

--- src/main/java/admin/adminsiteserver/qna/application/dto/QnaResponse.java
+++ src/main/java/admin/adminsiteserver/qna/application/dto/QnaResponse.java
@@ -1,99 +1,49 @@
 package admin.adminsiteserver.qna.application.dto;
 
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
+import admin.adminsiteserver.qna.application.dto.answer.AnswerResponse;
 import admin.adminsiteserver.qna.domain.Qna;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
-
 @Getter
-@JsonInclude(NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
 public class QnaResponse {
     private Long id;
-    private String authorId;
+    private String authorEmail;
     private String authorName;
     private String title;
     private String content;
     private LocalDateTime createAt;
     private LocalDateTime lastModifiedAt;
     private List<FilePathDto> files;
-    private List<AnswerResponse> answers;
     private List<QuestionCommentResponse> comments;
-
-    @Builder
-    public QnaResponse(Long id, String authorId, String authorName, String title, String content, LocalDateTime createAt, LocalDateTime lastModifiedAt, List<FilePathDto> images, List<AnswerResponse> answers, List<QuestionCommentResponse> comments) {
-        this.id = id;
-        this.authorId = authorId;
-        this.authorName = authorName;
-        this.title = title;
-        this.content = content;
-        this.createAt = createAt;
-        this.lastModifiedAt = lastModifiedAt;
-        this.files = images;
-        this.answers = answers;
-        this.comments = comments;
-    }
+    private List<AnswerResponse> answers;
 
     public static QnaResponse from(Qna qna) {
-        List<FilePathDto> filePathDtos = new ArrayList<>();
-        List<AnswerResponse> answerResponses = new ArrayList<>();
-        List<QuestionCommentResponse> commentResponses = new ArrayList<>();
-        if (qna.getFiles() != null) {
-            filePathDtos = qna.getFiles().stream()
-                    .map(FilePathDto::from)
-                    .collect(Collectors.toList());
-        }
-
-        if (qna.getAnswers() != null) {
-            answerResponses = qna.getAnswers().stream()
-                    .map(AnswerResponse::from)
-                    .collect(Collectors.toList());
-        }
-
-        if (qna.getComments() != null) {
-            commentResponses = qna.getComments().stream()
-                    .map(QuestionCommentResponse::from)
-                    .collect(Collectors.toList());
-        }
-
-        return QnaResponse.builder()
-                .id(qna.getId())
-                .authorId(qna.getAuthorId())
-                .authorName(qna.getAuthorName())
-                .title(qna.getTitle())
-                .content(qna.getContent())
-                .createAt(qna.getCreatedAt())
-                .lastModifiedAt(qna.getModifiedAt())
-                .images(filePathDtos)
-                .answers(answerResponses)
-                .comments(commentResponses)
-                .build();
-    }
-
-    public static QnaResponse toInstanceOfList(Qna qna) {
-        List<FilePathDto> filePathDtos = new ArrayList<>();
-        if (qna.getFiles() != null) {
-            filePathDtos = qna.getFiles().stream()
-                    .map(FilePathDto::from)
-                    .collect(Collectors.toList());
-        }
-
-        return QnaResponse.builder()
-                .id(qna.getId())
-                .authorId(qna.getAuthorId())
-                .authorName(qna.getAuthorName())
-                .title(qna.getTitle())
-                .content(qna.getContent())
-                .createAt(qna.getCreatedAt())
-                .lastModifiedAt(qna.getModifiedAt())
-                .images(filePathDtos)
-                .build();
+        return new QnaResponse(
+                qna.getId(),
+                qna.getAuthorEmail(),
+                qna.getAuthorName(),
+                qna.getTitle(),
+                qna.getContent(),
+                qna.getCreatedAt(),
+                qna.getModifiedAt(),
+                qna.getFiles().getFiles().stream()
+                        .map(FilePathDto::from)
+                        .collect(Collectors.toList()),
+                qna.getComments().getComments().stream()
+                        .map(QuestionCommentResponse::from)
+                        .collect(Collectors.toList()),
+                qna.getAnswers().getAnswers().stream()
+                        .map(AnswerResponse::from)
+                        .collect(Collectors.toList())
+        );
     }
 }
