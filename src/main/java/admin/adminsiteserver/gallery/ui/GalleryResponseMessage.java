9e4cc7453d5ef23e6c7a4e4efc698d0593d16349
chkim <cndghks0213@gmail.com>
2022-04-26 05:39:54 +0900

    fix: 잘못된 응답 메시지 수정

--- src/main/java/admin/adminsiteserver/gallery/ui/GalleryResponseMessage.java
+++ src/main/java/admin/adminsiteserver/gallery/ui/GalleryResponseMessage.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum GalleryResponseMessage {
-    GALLERY_UPLOAD_SUCCESS("공지사항 등록 성공"),
-    GALLERY_UPDATE_SUCCESS("공지사항 수정 성공"),
-    GALLERY_DELETE_SUCCESS("공지사항 삭제 성공"),
-    GALLERY_FIND_SUCCESS("공지사항 조회 성공"),
-    GALLERY_FIND_ALL_SUCCESS("공지사항 목록 조회 성공"),
+    GALLERY_UPLOAD_SUCCESS("갤러리 등록 성공"),
+    GALLERY_UPDATE_SUCCESS("갤러리 수정 성공"),
+    GALLERY_DELETE_SUCCESS("갤러리 삭제 성공"),
+    GALLERY_FIND_SUCCESS("갤러리 조회 성공"),
+    GALLERY_FIND_ALL_SUCCESS("갤러리 목록 조회 성공"),
     COMMENT_UPLOAD_SUCCESS("댓글 등록 성공"),
     COMMENT_UPDATE_SUCCESS("댓글 수정 성공"),
     COMMENT_DELETE_SUCCESS("댓글 삭제 성공"),;
