13ee6b728a5e460eb2d454a039a1572743e80d47
chkim <cndghks0213@gmail.com>
2022-04-26 05:55:03 +0900

    feat: Question의 파일 경로를 추출하는 메서드 추가

--- src/main/java/admin/adminsiteserver/qna/ui/dto/BaseQnaRequest.java
+++ src/main/java/admin/adminsiteserver/qna/ui/dto/BaseQnaRequest.java
@@ -1,18 +1,31 @@
 package admin.adminsiteserver.qna.ui.dto;
 
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
+import admin.adminsiteserver.qna.domain.QuestionFilePath;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BaseQnaRequest {
+    @NotBlank(message = "제목은 빈칸일 수 없습니다.")
     private String title;
+
+    @NotBlank(message = "내용은 빈칸일 수 없습니다.")
     private String content;
     private List<FilePathDto> files;
+
+    public List<QuestionFilePath> toQuestionFilePaths() {
+        if (files == null) return new ArrayList<>();
+        return files.stream()
+                .map(filePathDto -> filePathDto.toFilePath(QuestionFilePath.class))
+                .collect(Collectors.toList());
+    }
 }
