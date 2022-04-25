69c91303ef98b96313c28818e1c6b46f4c384a68
chkim <cndghks0213@gmail.com>
2022-04-26 05:59:37 +0900

    refactor: file, comment에 대한 일급 컬렉션을 사용함에 따른 코드 수정

--- src/main/java/admin/adminsiteserver/qna/application/dto/answer/AnswerResponse.java
+++ src/main/java/admin/adminsiteserver/qna/application/dto/answer/AnswerResponse.java
@@ -1,15 +1,17 @@
-package admin.adminsiteserver.qna.application.dto;
+package admin.adminsiteserver.qna.application.dto.answer;
 
 import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
-import admin.adminsiteserver.qna.domain.Answer;
+import admin.adminsiteserver.qna.domain.answer.Answer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AnswerResponse {
     private Long id;
@@ -24,15 +26,15 @@ public class AnswerResponse {
     public static AnswerResponse from(Answer answer) {
         return new AnswerResponse(
                 answer.getId(),
-                answer.getAuthorId(),
+                answer.getAuthorEmail(),
                 answer.getAuthorName(),
                 answer.getContent(),
                 answer.getCreatedAt(),
                 answer.getModifiedAt(),
-                answer.getFiles().stream()
+                answer.getFiles().getFiles().stream()
                         .map(FilePathDto::from)
                         .collect(Collectors.toList()),
-                answer.getComments().stream()
+                answer.getComments().getComments().stream()
                         .map(AnswerCommentResponse::from)
                         .collect(Collectors.toList())
         );
