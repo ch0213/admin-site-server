754e37b92a46238e9098c193cd23de2ab7fe98d0
chkim <cndghks0213@gmail.com>
2022-04-26 05:52:18 +0900

    refactor: 회원 관련 요청에 검증 추가

--- src/main/java/admin/adminsiteserver/member/member/ui/dto/SignUpRequest.java
+++ src/main/java/admin/adminsiteserver/member/member/ui/dto/SignUpRequest.java
@@ -7,16 +7,29 @@ import lombok.Getter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @AllArgsConstructor
 public class SignUpRequest {
 
-    private String userId;
-    private String password;
+    @Email
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
+
+    @NotBlank(message = "학번을 입력해주세요.")
     private String studentNumber;
+
+    @NotBlank(message = "연락처를 입력해주세요.")
     private String phoneNumber;
+
     private MultipartFile image;
 
     public boolean hasImage() {
@@ -25,9 +38,8 @@ public class SignUpRequest {
 
     public Member toMember(PasswordEncoder passwordEncoder) {
         return Member.builder()
-                .userId(userId)
-                .password(passwordEncoder.encode(password))
                 .email(email)
+                .password(passwordEncoder.encode(password))
                 .name(name)
                 .studentNumber(studentNumber)
                 .phoneNumber(phoneNumber)
