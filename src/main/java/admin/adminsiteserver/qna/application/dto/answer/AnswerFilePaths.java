527350bf410418246cadba95dd8f2e244a4e5b30
chkim <cndghks0213@gmail.com>
2022-04-26 05:53:17 +0900

    feat: AnswerFilePath를 관리하는 일급 컬렉션 생성

---
+++ src/main/java/admin/adminsiteserver/qna/application/dto/answer/AnswerFilePaths.java
@@ -0,0 +1,43 @@
+package admin.adminsiteserver.qna.application.dto.answer;
+
+import admin.adminsiteserver.common.aws.infrastructure.dto.FilePathDto;
+import admin.adminsiteserver.qna.domain.answer.AnswerFilePath;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+public class AnswerFilePaths {
+
+    @OneToMany(cascade = ALL, orphanRemoval = true)
+    @JoinColumn(name = "answer_id")
+    private List<AnswerFilePath> files = new ArrayList<>();
+
+    public void saveFilePaths(List<AnswerFilePath> filePaths) {
+        files.addAll(filePaths);
+    }
+
+    public void deleteFiles(List<FilePathDto> deleteFileUrls) {
+        if (deleteFileUrls == null) return;
+        files.removeIf(filePath -> deleteFileUrls.stream().map(FilePathDto::getFileUrl)
+                .collect(Collectors.toList())
+                .contains(filePath.getFileUrl()));
+    }
+
+    public List<FilePathDto> findDeleteFilePaths() {
+        return files.stream()
+                .map(FilePathDto::from)
+                .collect(Collectors.toList());
+    }
+}
