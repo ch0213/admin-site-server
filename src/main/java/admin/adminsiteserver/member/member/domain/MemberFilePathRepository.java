9c2771c4a1576cfe028b99bba385dfe6415b57af
chkim <cndghks0213@gmail.com>
2022-06-03 20:06:15 +0900

    refactor: 불필요한 memberFileRepository 제거

--- src/main/java/admin/adminsiteserver/member/member/domain/MemberFilePathRepository.java
+++
@@ -1,6 +0,0 @@
-package admin.adminsiteserver.member.member.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberFilePathRepository extends JpaRepository<MemberFilePath, Long> {
-}
