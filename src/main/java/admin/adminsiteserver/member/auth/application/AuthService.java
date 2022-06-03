0ee907beeb42e0cab6f1f4bb763a61abec05c32d
chkim <cndghks0213@gmail.com>
2022-06-04 04:54:19 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/admin/adminsiteserver/member/auth/application/AuthService.java
+++ src/main/java/admin/adminsiteserver/member/auth/application/AuthService.java
@@ -8,7 +8,6 @@ import admin.adminsiteserver.member.auth.util.JwtTokenProvider;
 import admin.adminsiteserver.member.member.domain.Member;
 import admin.adminsiteserver.member.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +18,6 @@ import java.util.Optional;
 
 import static admin.adminsiteserver.member.member.domain.RoleType.*;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
