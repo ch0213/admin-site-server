47d8a6aacfc0d5fd8106597b909656691ca2199d
chkim <cndghks0213@gmail.com>
2022-06-03 20:03:27 +0900

    refactor: httpStatus를 final로 변경

--- src/main/java/admin/adminsiteserver/aws/exception/AwsExceptionType.java
+++ src/main/java/admin/adminsiteserver/aws/exception/AwsExceptionType.java
@@ -1,16 +1,16 @@
 package admin.adminsiteserver.aws.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum AwsExceptionType {
     CONVERT_FILE_EXCEPTION("파일을 변환할 수 없습니다.", INTERNAL_SERVER_ERROR);
 
     private final String message;
-    private HttpStatus status;
+    private final HttpStatus status;
 }
