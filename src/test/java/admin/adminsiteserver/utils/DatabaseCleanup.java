dc24448a8ee1fbc148c1b5d7ab0e89a6a55cf85e
chkim <cndghks0213@gmail.com>
2022-08-29 23:57:31 +0900

    refactor: DatabaseCleanup 리팩토링

--- src/test/java/admin/adminsiteserver/utils/DatabaseCleanup.java
+++ src/test/java/admin/adminsiteserver/utils/DatabaseCleanup.java
@@ -1,19 +1,18 @@
 package admin.adminsiteserver.utils;
 
-import com.google.common.base.CaseFormat;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Service;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import javax.persistence.metamodel.EntityType;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-@Service
-@ActiveProfiles("test")
+@Profile("test")
+@Component
 public class DatabaseCleanup implements InitializingBean {
     @PersistenceContext
     private EntityManager entityManager;
@@ -23,21 +22,33 @@ public class DatabaseCleanup implements InitializingBean {
     @Override
     public void afterPropertiesSet() {
         tableNames = entityManager.getMetamodel().getEntities().stream()
-                .filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
-                .map(e -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.getName()))
+                .map(this::extractTableName)
                 .collect(Collectors.toList());
+    }
+
+    private String extractTableName(EntityType<?> entity) {
+        Table declaredAnnotation = entity.getJavaType().getDeclaredAnnotation(Table.class);
+        if (declaredAnnotation == null) {
+            return convertCamelToSnake(entity.getName());
+        }
+        return declaredAnnotation.name();
+    }
+
+    private String convertCamelToSnake(String str){
+        String regex = "([a-z])([A-Z])";
+        String replacement = "$1_$2";
+        return str.replaceAll(regex, replacement).toLowerCase();
     }
 
     @Transactional
     public void execute() {
         entityManager.flush();
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
-
         for (String tableName : tableNames) {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1")
+                    .executeUpdate();
         }
-
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
     }
 }
