4e14dbd43fd77eda183c7ab7db327f7352aff4bc
chkim <cndghks0213@gmail.com>
2022-09-01 01:10:08 +0900

    test: 회원 단위 테스트 리팩토링

--- src/test/java/admin/adminsiteserver/member/domain/MemberTest.java
+++ src/test/java/admin/adminsiteserver/member/domain/MemberTest.java
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static admin.adminsiteserver.member.domain.RoleType.MEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("회원 도메인 테스트")
 class MemberTest {
-
     private Member member;
 
     @BeforeEach
@@ -31,18 +31,13 @@ class MemberTest {
     @DisplayName("회원 정보(이름, 학번, 연락처)를 업데이트 한다.")
     @Test
     void updateMemberInfo() {
-        member.updateMemberInfo("김짱구", "201700000", "010-1111-1111");
+        member.update("김짱구", "201700000", "010-1111-1111");
 
-        Member testMember = Member.builder()
-                .email("admin@admin.com")
-                .password("admin")
-                .name("김짱구")
-                .studentNumber("201700000")
-                .phoneNumber("010-1111-1111")
-                .filePath(new MemberFilePath("치킨.png", "https://cloudfront.com/admin"))
-                .role(MEMBER)
-                .build();
-        assertThat(member).isEqualTo(testMember);
+        assertAll(
+                () -> assertThat(member.getName()).isEqualTo("김짱구"),
+                () -> assertThat(member.sameStudentNumber("201700000")).isTrue(),
+                () -> assertThat(member.getPhoneNumber()).isEqualTo("010-1111-1111")
+        );
     }
 
     @DisplayName("회원의 비밀번호를 업데이트 한다.")
@@ -56,9 +51,10 @@ class MemberTest {
     @DisplayName("회원의 프로필 이미지를 업데이트 한다.")
     @Test
     void uploadProfileImage() {
-        member.uploadProfileImage(new MemberFilePath("피자.png", "https://cloudfront.com/admin2"));
+        member.updateImage("피자.png", "https://cloudfront.com/admin2");
 
-        assertThat(member.getFilePath()).isEqualTo(new MemberFilePath("피자.png", "https://cloudfront.com/admin2"));
+        assertThat(member.getFilePath())
+                .isEqualTo(new MemberFilePath("피자.png", "https://cloudfront.com/admin2"));
     }
 
     @DisplayName("회원의 역할을 업데이트 한다.")
@@ -76,24 +72,33 @@ class MemberTest {
     void securityRoleType(RoleType roleType) {
         member.updateRole(roleType);
 
-        assertThat(member.securityRoleType()).isEqualTo(roleType.getRole());
+        assertThat(member.getRoleType()).isEqualTo(roleType.getRole());
     }
 
-    @DisplayName("회원의 이메일 존재 여부를 반환한다.")
+    @DisplayName("회원의 이메일이 존재할 경우 True를 반환한다.")
+    @Test
+    void hasEmail() {
+        member = Member.builder()
+                .email("admin@admin.com")
+                .build();
+
+        assertThat(member.hasEmail()).isTrue();
+    }
+
+    @DisplayName("회원의 이메일이 존재하지 않을 경우 False를 반환한다.")
+    @NullAndEmptySource
     @ParameterizedTest
-    @CsvSource(value = {"admin@admin.com:true", "null:false"}, nullValues = "null", delimiter = ':')
-    void hasEmail(String email, boolean hasEmail) {
-        Member testMember = Member.builder()
+    void hasNotEmail(String email) {
+        member = Member.builder()
                 .email(email)
                 .build();
 
-        assertThat(testMember.hasEmail()).isEqualTo(hasEmail);
+        assertThat(member.hasEmail()).isFalse();
     }
 
-    @DisplayName("회원의 삭제 여부를 true로 변경한다. 회원의 삭제 여부를 반환한다.")
+    @DisplayName("회원을 삭제한다.")
     @Test
-    void delete_isDeleted() {
-        assertThat(member.isDeleted()).isFalse();
+    void delete() {
         member.delete();
         assertThat(member.isDeleted()).isTrue();
     }
