c6038eb8c6c369d345b87133f6a21001edf2322c
chkim <cndghks0213@gmail.com>
2022-08-31 22:53:50 +0900

    feat: 로그인 기능 구현

---
+++ src/main/java/admin/adminsiteserver/authentication/ui/AuthenticationController.java
@@ -0,0 +1,22 @@
+package admin.adminsiteserver.authentication.ui;
+
+import admin.adminsiteserver.authentication.application.AuthenticationService;
+import admin.adminsiteserver.authentication.dto.response.LoginResponse;
+import admin.adminsiteserver.authentication.dto.request.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
