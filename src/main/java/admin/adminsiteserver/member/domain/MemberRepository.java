f7c5dc435400927b5f28afb0fa26f6d74abdfb3c
chkim <cndghks0213@gmail.com>
2022-08-31 23:43:38 +0900

    feat: 삭제되지 않은 회원을 조회하는 기능 추가

--- src/main/java/admin/adminsiteserver/member/domain/MemberRepository.java
+++ src/main/java/admin/adminsiteserver/member/domain/MemberRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByIdAndAndDeletedFalse(Long id);
+
     Optional<Member> findByEmail(String email);
 
     Page<Member> findAllByDeletedFalse(Pageable pageable);
