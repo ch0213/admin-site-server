61c1cde28a88721c828df0c7714957739ed80f20
chkim <cndghks0213@gmail.com>
2022-09-03 23:24:36 +0900

    style: 개행 추가

--- src/test/java/admin/adminsiteserver/AcceptanceTestSteps.java
+++ src/test/java/admin/adminsiteserver/AcceptanceTestSteps.java
@@ -3,13 +3,15 @@ package admin.adminsiteserver;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 
-public class AcceptanceSteps {
+public class AcceptanceTestSteps {
     public static RequestSpecification given() {
-        return RestAssured.given().log().all();
+        return RestAssured
+                .given().log().all();
     }
 
     public static RequestSpecification given(String token) {
-        return RestAssured.given().log().all()
+        return RestAssured
+                .given().log().all()
                 .auth().oauth2(token);
     }
 }
