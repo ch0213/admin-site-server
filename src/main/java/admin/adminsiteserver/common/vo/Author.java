5cc5252950b63021034e65663aef26c188bc8fa5
chkim <cndghks0213@gmail.com>
2022-10-11 12:01:09 +0900

    feat: 공통으로 사용할 Author 생성

---
+++ src/main/java/admin/adminsiteserver/common/vo/Author.java
@@ -0,0 +1,84 @@
+package admin.adminsiteserver.common.vo;
+
+import admin.adminsiteserver.common.domain.RoleType;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Getter
+@Embeddable
+public class Author implements Comparable<Author> {
+    @Column
+    private Long authorId;
+
+    @Column
+    private String authorEmail;
+
+    @Column
+    private String authorStudentNumber;
+
+    @Column
+    private String authorName;
+
+    @Column
+    private RoleType roleType;
+
+    protected Author() {
+    }
+
+    public Author(Long authorId, String authorEmail, String authorStudentNumber, String authorName, RoleType roleType) {
+        this.authorId = authorId;
+        this.authorEmail = authorEmail;
+        this.authorStudentNumber = authorStudentNumber;
+        this.authorName = authorName;
+        this.roleType = roleType;
+    }
+
+    public boolean isAdmin() {
+        return roleType == RoleType.ADMIN;
+    }
+
+    public boolean isPresident() {
+        return roleType == RoleType.PRESIDENT;
+    }
+
+    public boolean isOfficer() {
+        return roleType == RoleType.OFFICER;
+    }
+
+    public boolean isMember() {
+        return roleType == RoleType.MEMBER;
+    }
+
+    public boolean isGuest() {
+        return roleType == RoleType.GUEST;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return Objects.equals(getAuthorId(), author.getAuthorId())
+                && Objects.equals(getAuthorEmail(), author.getAuthorEmail())
+                && Objects.equals(getAuthorStudentNumber(), author.getAuthorStudentNumber())
+                && Objects.equals(getAuthorName(), author.getAuthorName())
+                && getRoleType() == author.getRoleType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAuthorId(), getAuthorEmail(), getAuthorStudentNumber(), getAuthorName(), getRoleType());
+    }
+
+    @Override
+    public int compareTo(Author o) {
+        return this.roleType.compareTo(o.roleType);
+    }
+}
