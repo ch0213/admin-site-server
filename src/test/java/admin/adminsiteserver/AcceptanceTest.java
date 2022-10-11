93644e55ffd2ac2534bf49205a2cd8323b09478b
chkim <cndghks0213@gmail.com>
2022-10-11 11:55:52 +0900

    test: 테스트를 위한 회원 데이터 생성 기능 추가

--- src/test/java/admin/adminsiteserver/AcceptanceTest.java
+++ src/test/java/admin/adminsiteserver/AcceptanceTest.java
@@ -1,6 +1,8 @@
 package admin.adminsiteserver;
 
 import admin.adminsiteserver.aws.infrastructure.S3Uploader;
+import admin.adminsiteserver.member.domain.Member;
+import admin.adminsiteserver.member.domain.MemberRepository;
 import admin.adminsiteserver.utils.DatabaseCleanup;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,7 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static admin.adminsiteserver.member.fixture.MemberFixture.*;
+import static admin.adminsiteserver.member.fixture.MemberFixture.회원2;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,6 +26,12 @@ public class AcceptanceTest {
 
     @Autowired
     private DatabaseCleanup databaseCleanup;
+
+    @Autowired
+    protected MemberRepository memberRepository;
+
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
 
     @MockBean
     protected S3Uploader s3Uploader;
@@ -29,5 +43,16 @@ public class AcceptanceTest {
             databaseCleanup.afterPropertiesSet();
         }
         databaseCleanup.execute();
+        saveMembers();
+    }
+
+    private void saveMembers() {
+        List<Member> members = List.of(
+                관리자1.toEntity(passwordEncoder), 관리자2.toEntity(passwordEncoder),
+                회장1.toEntity(passwordEncoder), 회장2.toEntity(passwordEncoder),
+                임원1.toEntity(passwordEncoder), 임원2.toEntity(passwordEncoder),
+                회원1.toEntity(passwordEncoder), 회원2.toEntity(passwordEncoder)
+        );
+        memberRepository.saveAll(members);
     }
 }
