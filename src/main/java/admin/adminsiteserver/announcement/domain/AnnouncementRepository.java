f76e6b1e11b875d5326d09192bd82da5b5474dc8
chkim <cndghks0213@gmail.com>
2022-10-12 12:24:25 +0900

    feat: 삭제되지 않은 모든 공지사항을 조회하는 기능 추가

--- src/main/java/admin/adminsiteserver/announcement/domain/AnnouncementRepository.java
+++ src/main/java/admin/adminsiteserver/announcement/domain/AnnouncementRepository.java
@@ -3,6 +3,7 @@ package admin.adminsiteserver.announcement.domain;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AnnouncementRepository {
@@ -19,4 +20,6 @@ public interface AnnouncementRepository {
     Page<Announcement> findAll(Pageable pageable);
 
     Page<Announcement> findAllByDeletedIsFalse(Pageable pageable);
+
+    List<Announcement> findAllByDeletedIsFalse();
 }
