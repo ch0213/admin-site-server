ea1ba3123ac2dbcc3911bde06b8b888225a7c3b8
chkim <cndghks0213@gmail.com>
2022-10-28 23:01:19 +0900

    feat: Thread pool 설정

---
+++ src/main/java/admin/adminsiteserver/common/config/AsyncConfig.java
@@ -0,0 +1,21 @@
+package admin.adminsiteserver.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("admin-site-server-async-");
+        return executor;
+    }
+}
