4b5a9b40f7d72664dd344a81fe9f6629eaa80cd6
chkim <cndghks0213@gmail.com>
2022-11-20 02:16:18 +0900

    feat: 일정 관련 예외 생성

---
+++ src/main/java/admin/adminsiteserver/calendar/exception/CalendarPeriodException.java
@@ -0,0 +1,14 @@
+package admin.adminsiteserver.calendar.exception;
+
+import admin.adminsiteserver.common.exception.BaseException;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CalendarPeriodException extends BaseException {
+    public CalendarPeriodException() {
+        super("종료일은 시작일 이후여야 합니다.", LocalDateTime.now(), HttpStatus.BAD_REQUEST);
+    }
+}
