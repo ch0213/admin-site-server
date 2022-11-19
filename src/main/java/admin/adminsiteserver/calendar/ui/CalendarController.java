36a22819986690ed62aff2b691f44ddcc0393939
chkim <cndghks0213@gmail.com>
2022-11-20 02:18:30 +0900

    feat: 일정 관련 기능 구현

--- src/main/java/admin/adminsiteserver/calendar/ui/CalendarController.java
+++ src/main/java/admin/adminsiteserver/calendar/ui/CalendarController.java
@@ -1,48 +1,59 @@
 package admin.adminsiteserver.calendar.ui;
 
+import admin.adminsiteserver.authentication.domain.LoginMember;
+import admin.adminsiteserver.authentication.ui.AuthenticationPrincipal;
 import admin.adminsiteserver.calendar.application.CalendarService;
-import admin.adminsiteserver.calendar.application.dto.CalendarResponse;
-import admin.adminsiteserver.calendar.ui.dto.CalendarRequest;
-import admin.adminsiteserver.calendar.ui.dto.InquireCalendarRequest;
-import admin.adminsiteserver.common.response.CommonResponse;
+import admin.adminsiteserver.calendar.domain.Author;
+import admin.adminsiteserver.calendar.ui.request.CalendarFindRequest;
+import admin.adminsiteserver.calendar.ui.request.CalendarRequest;
+import admin.adminsiteserver.calendar.ui.response.CalendarResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
-import static admin.adminsiteserver.calendar.ui.CalendarResponseMessage.*;
-
 @RestController
-@RequestMapping("/calendars")
 @RequiredArgsConstructor
 public class CalendarController {
-
     private final CalendarService calendarService;
 
-    @PostMapping
-    public CommonResponse<CalendarResponse> uploadCalendar(@Valid @RequestBody CalendarRequest calendarRequest) {
-        CalendarResponse calendarResponse = calendarService.uploadCalendar(calendarRequest);
-        return CommonResponse.of(calendarResponse, CALENDAR_REGISTER_SUCCESS.getMessage());
-    }
-
-    @PutMapping("/{calendarId}")
-    public CommonResponse<CalendarResponse> updateCalendar(
-            @Valid @RequestBody CalendarRequest calendarRequest,
-            @PathVariable Long calendarId
+    @PostMapping("/calendars")
+    public ResponseEntity<Void> create(
+            @Valid @RequestBody CalendarRequest request,
+            @AuthenticationPrincipal LoginMember loginMember
     ) {
-        CalendarResponse calendarResponse = calendarService.updateCalendar(calendarRequest, calendarId);
-        return CommonResponse.of(calendarResponse, CALENDAR_UPDATE_SUCCESS.getMessage());
+        Long calendarId = calendarService.upload(request, loginMember.toAuthor(Author::new));
+        return ResponseEntity.created(URI.create("/calendars/" + calendarId)).build();
     }
 
-    @DeleteMapping("/{calendarId}")
-    public CommonResponse<Void> deleteCalendar(@PathVariable Long calendarId) {
-        calendarService.deleteCalendar(calendarId);
-        return CommonResponse.from(CALENDAR_DELETE_SUCCESS.getMessage());
+    @GetMapping("/calendars")
+    public ResponseEntity<List<CalendarResponse>> find(@Valid CalendarFindRequest request) {
+        List<CalendarResponse> response = calendarService.findAll(request.getYear(), request.getMonth());
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping
-    public CommonResponse<List<CalendarResponse>> inquireCalendar(@Valid InquireCalendarRequest request) {
-        return CommonResponse.of(calendarService.findCalendars(request), CALENDAR_LIST_INQUIRE_SUCCESS.getMessage());
+    @GetMapping("/calendars/{calendarId}")
+    public ResponseEntity<CalendarResponse> find(@PathVariable Long calendarId) {
+        CalendarResponse response = calendarService.find(calendarId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/calendars/{calendarId}")
+    public ResponseEntity<Void> update(
+            @PathVariable Long calendarId,
+            @Valid @RequestBody CalendarRequest request,
+            @AuthenticationPrincipal LoginMember loginMember
+    ) {
+        calendarService.update(calendarId, request, loginMember.toAuthor(Author::new));
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/calendars/{calendarId}")
+    public ResponseEntity<Void> delete(@PathVariable Long calendarId, @AuthenticationPrincipal LoginMember loginMember) {
+        calendarService.delete(calendarId, loginMember.toAuthor(Author::new));
+        return ResponseEntity.noContent().build();
     }
 }
