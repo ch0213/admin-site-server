ab95751d39b31117a7698ecd4137cd2d079ce46c
ch0213 <cndghks0213@gmail.com>
2022-12-16 21:33:03 +0900

    feat: 권한 신청 승인 이벤트를 구독해 회원의 권한을 변경하는 기능 구현

--- src/main/java/admin/adminsiteserver/member/application/MemberEventListener.java
+++ src/main/java/admin/adminsiteserver/member/application/MemberEventListener.java
@@ -1,24 +1,47 @@
 package admin.adminsiteserver.member.application;
 
+import admin.adminsiteserver.member.domain.Member;
 import admin.adminsiteserver.member.domain.MemberEvent;
 import admin.adminsiteserver.member.domain.MemberEventHistory;
 import admin.adminsiteserver.member.domain.MemberEventHistoryRepository;
+import admin.adminsiteserver.member.domain.MemberRepository;
+import admin.adminsiteserver.member.exception.MemberNotFoundException;
+import admin.adminsiteserver.promotion.domain.PromotionEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import static admin.adminsiteserver.promotion.domain.PromotionStatus.APPROVE;
 import static org.springframework.transaction.event.TransactionPhase.BEFORE_COMMIT;
 
 @Component
 @RequiredArgsConstructor
 public class MemberEventListener {
     private final MemberEventHistoryRepository memberEventHistoryRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     @TransactionalEventListener(phase = BEFORE_COMMIT)
     public void handle(MemberEvent event) {
         MemberEventHistory history = event.history();
         memberEventHistoryRepository.save(history);
+    }
+
+    @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener
+    public void promotionEvent(PromotionEvent event) {
+        if (event.getStatus() == APPROVE) {
+            Member member = findById(event.getMemberId());
+            member.updateRole(event.getRole());
+        }
+    }
+
+    private Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(MemberNotFoundException::new);
     }
 }
