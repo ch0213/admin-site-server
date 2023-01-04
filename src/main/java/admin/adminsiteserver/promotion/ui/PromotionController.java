3910f9b894e6acc1e6ebec856e4df1f53a9e5365
ch0213 <cndghks0213@gmail.com>
2023-01-05 06:42:05 +0900

    refactor: @RequestMapping으로 중복 제거

--- src/main/java/admin/adminsiteserver/promotion/ui/PromotionController.java
+++ src/main/java/admin/adminsiteserver/promotion/ui/PromotionController.java
@@ -3,7 +3,6 @@ package admin.adminsiteserver.promotion.ui;
 import admin.adminsiteserver.authentication.domain.LoginMember;
 import admin.adminsiteserver.promotion.application.PromotionService;
 import admin.adminsiteserver.promotion.domain.Author;
-import admin.adminsiteserver.promotion.ui.request.PromotionCursor;
 import admin.adminsiteserver.promotion.ui.request.PromotionFindRequest;
 import admin.adminsiteserver.promotion.ui.response.PromotionResponse;
 import admin.adminsiteserver.promotion.ui.request.PromotionProcessRequest;
@@ -20,56 +19,57 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/promotions")
 public class PromotionController {
     private final PromotionService promotionService;
 
-    @PostMapping("/promotions")
+    @PostMapping
     public ResponseEntity<Void> register(@Valid @RequestBody PromotionRequest request,
                                          @AuthenticationPrincipal LoginMember loginMember) {
         Long promotionId = promotionService.apply(request.role(), loginMember.toAuthor(Author::new));
         return ResponseEntity.created(URI.create("/promotions/" + promotionId)).build();
     }
 
-    @PutMapping("/promotions/{promotionId}")
+    @PutMapping("/{promotionId}")
     public void update(@PathVariable Long promotionId,
                        @Valid @RequestBody PromotionRequest request,
                        @AuthenticationPrincipal LoginMember loginMember) {
         promotionService.update(promotionId, request.role(), loginMember.toAuthor(Author::new));
     }
 
-    @DeleteMapping("/promotions/{promotionId}")
+    @DeleteMapping("/{promotionId}")
     public ResponseEntity<Void> delete(@PathVariable Long promotionId,
                                        @AuthenticationPrincipal LoginMember loginMember) {
         promotionService.cancel(promotionId, loginMember.toAuthor(Author::new));
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/promotions/{promotionId}/approve")
+    @PostMapping("/{promotionId}/approve")
     public void approve(@PathVariable Long promotionId) {
         promotionService.approve(promotionId);
     }
 
-    @PostMapping("/promotions/approve")
+    @PostMapping("/approve")
     public void approveAll(@Valid @RequestBody PromotionProcessRequest request) {
         promotionService.approveAll(request.getPromotionIds());
     }
 
-    @PostMapping("/promotions/{promotionId}/reject")
+    @PostMapping("/{promotionId}/reject")
     public void reject(@PathVariable Long promotionId) {
         promotionService.reject(promotionId);
     }
 
-    @PostMapping("/promotions/reject")
+    @PostMapping("/reject")
     public void rejectAll(@Valid @RequestBody PromotionProcessRequest request) {
         promotionService.rejectAll(request.getPromotionIds());
     }
 
-    @GetMapping("/promotions")
+    @GetMapping
     public List<PromotionResponse> findAll(PromotionFindRequest request, Pageable pageable) {
         return promotionService.findAll(request, pageable);
     }
 
-    @GetMapping("/promotions/me")
+    @GetMapping("/me")
     public List<PromotionResponse> myPromotions(@AuthenticationPrincipal LoginMember loginMember,
                                                 PromotionFindRequest request,
                                                 Pageable pageable) {
