6954f617b235c41fed56c3831bc17468c38ce69e
ch0213 <cndghks0213@gmail.com>
2023-01-25 22:55:19 +0900

    feat: NoArgsConstructor 추가

--- src/main/java/admin/adminsiteserver/common/response/PageInformation.java
+++ src/main/java/admin/adminsiteserver/common/response/PageInformation.java
@@ -1,17 +1,25 @@
 package admin.adminsiteserver.common.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class PageInformation {
     private int currentPage;
     private int totalPages;
     private int numberOfElements;
     private int pageSize;
     private int totalElements;
+
+    public PageInformation(int currentPage, int totalPages, int numberOfElements, int pageSize, int totalElements) {
+        this.currentPage = currentPage;
+        this.totalPages = totalPages;
+        this.numberOfElements = numberOfElements;
+        this.pageSize = pageSize;
+        this.totalElements = totalElements;
+    }
 
     public static <T> PageInformation from(Page<T> page) {
         return new PageInformation(
